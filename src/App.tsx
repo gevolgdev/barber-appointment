26c7dac8d1caeda80d50d13b1e2562b798ac6193
Pinelli <ricardosilvapinelli@gmail.com>
2023-05-02 17:08:48 -0300

    App: Outlet & GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
 // import { useState} from 'react';
 
-interface UserProps {
-  name: string;
-
-}
+import { Outlet } from "react-router-dom"
+import { GlobalStyle } from "./style/GlobalStyle"
 
 function App() {
 
-  const user: UserProps = {
-    name: 'Ricardo',
-  }
-
-  const handleShare = async () => {
-    try {
-      await navigator.share({
-        title: `Corte marcado de ${user.name}`,
-        text: ` Dia: 02/05 || Horario: 14h30 || Corte: Navalhado || Valor: R$30,00`,
-        url: 'https://github.com/gevolgdev',
-      })
-    } catch (err) {
-      console.log('O compartilhamento falhou!' + err);
-    }
-  }
-
   return (
     <>
-      <button onClick={handleShare}>Compartilhar</button>
+      <Outlet/>
+      <GlobalStyle/>
     </>
   )
 }
