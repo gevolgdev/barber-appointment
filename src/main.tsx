04d009138b80d52a62dad76d1b9593713ee52d5a
Pinelli <ricardosilvapinelli@gmail.com>
2023-05-02 17:08:11 -0300

    Main: Routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Home, NewAppointment } from './routes/index.ts';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {
+        path: '/',
+        element: <Home/>
+      },
+      {
+        path: '/marcar',
+        element: <NewAppointment/>
+      },
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
