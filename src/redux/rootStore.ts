aeb115819aa3b17ecc6d5fc8cd1605657b5907bf
Pinelli <ricardosilvapinelli@gmail.com>
2023-05-03 14:35:42 -0300

    Redux: Store

--- src/redux/rootStore.ts
+++ src/redux/rootStore.ts
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
+import appointmentSlice from './slices/appointmentSlice';
 
 
 export const rootStore = combineReducers({
+  appointmentSlice: appointmentSlice,
+});
 
-})
-
-export type RootState = ReturnType<typeof rootStore>
+export type RootState = ReturnType<typeof rootStore>;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { rootStore } from "./rootStore";
 
 const store = configureStore({
-  reducer: {}
+  reducer: rootStore,
 });
 
 export default store;
