c89372767854ef843aa1afb2f59728ba3162c0af
Pinelli <ricardosilvapinelli@gmail.com>
2023-05-03 13:33:49 -0300

    Header: Component & Design

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import * as S from './style';
+
+const Header = () => {
+  return (
+    <S.Container>
+      <h1>
+        GEVOLG
+        <span>BARBER</span>
+      </h1>
+    </S.Container>
+  )
+}
+
+export default Header
---
+++ src/components/Header/style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding: 40px 0;
+
+  h1 {
+    font-weight: 800;
+    color: #1f1f1f;
+
+    span {
+      font-weight: 400;
+    }
+  }
+`
