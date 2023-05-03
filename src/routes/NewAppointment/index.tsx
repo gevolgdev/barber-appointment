99d63c2f21fc60389e238f9ea7ad8dee3db2f7d7
Pinelli <ricardosilvapinelli@gmail.com>
2023-05-03 16:10:57 -0300

    NewAppointment: dispatch the data

--- src/routes/NewAppointment/index.tsx
+++ src/routes/NewAppointment/index.tsx
@@ -2,20 +2,18 @@ import { ChangeEvent, MouseEvent, useState } from 'react';
 import { Header } from '../../components';
 import Selects from './Selects';
 import * as S from './style';
-
-interface AppointmentProps {
-  name: string;
-  barber: string;
-  cut: string;
-  add: string;
-  hour: string;
-}
+import { AppointmentProps } from '../../types/types';
+import { useDispatch } from 'react-redux';
+import { newAppointmentReducer } from '../../redux/slices/appointmentSlice';
+import { useNavigate } from 'react-router-dom';
 
 const NewAppointment = () => {
 
   const INITIAL_STATE = {name: '', barber: '', cut: '', add: '', hour: ''}
   const [appointment, setAppointment] = useState<AppointmentProps>(INITIAL_STATE);
-  console.log(appointment);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleAddAppointment = (event: ChangeEvent<HTMLInputElement | HTMLSelectElement>): void => {
     const { id, value } = event.target;
@@ -24,6 +22,8 @@ const NewAppointment = () => {
 
   const handleConfirm = (event: MouseEvent<HTMLButtonElement>): void => {
     event.preventDefault();
+    dispatch(newAppointmentReducer(appointment))
+    navigate('/');
   };
 
   return (
