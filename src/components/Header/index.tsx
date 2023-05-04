ca2635413e0383e2175c2a548247d8ef2aefe3e7
Pinelli <ricardosilvapinelli@gmail.com>
2023-05-04 12:18:30 -0300

    Header: BackToHome button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,27 @@
 import * as S from './style';
+import { BsArrowLeftShort } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
 
-const Header = () => {
+interface HeaderProps {
+  backToHome?: boolean;
+};
+
+const Header: React.FC<HeaderProps> = ({backToHome = false}) => {
+
+  const navigate = useNavigate();
+  const handleClick = (): void => navigate('/');
+
   return (
     <S.Container>
+      {backToHome && 
+        <S.BackButton onClick={handleClick}><BsArrowLeftShort/> Voltar</S.BackButton>
+      }
       <h1>
         GEVOLG
         <span>BARBER</span>
       </h1>
     </S.Container>
   )
-}
+};
 
-export default Header
+export default Header;
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 
 export const Container = styled.header`
+  position: relative;
   display: flex;
+  flex-direction: row;
   justify-content: center;
   width: 100%;
   padding: 40px 0;
@@ -15,3 +17,20 @@ export const Container = styled.header`
     }
   }
 `
+
+export const BackButton = styled.button`
+  position: absolute;
+  left: 0;
+  display: flex;
+  align-items: center;
+  background: transparent;
+  border: none;
+  font-size: 1rem;
+  font-family: 'Inter Tight';
+  cursor: pointer;
+
+  svg {
+    margin-right: 10px;
+    font-size: 2rem;
+  }
+`;
