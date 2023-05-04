6afbb78dc599871508562a2e5d6066d7525bf45c
Pinelli <ricardosilvapinelli@gmail.com>
2023-05-03 21:57:33 -0300

    Confirm: Code & Design

--- src/routes/Confirm/index.tsx
+++ src/routes/Confirm/index.tsx
@@ -1,9 +1,46 @@
+import { Header } from "../../components";
+import { RootState } from "../../redux/rootStore";
+import { AppointmentProps } from "../../types/types";
 import * as S from "./style";
-
+import { useSelector } from 'react-redux';
 
 const Confirm = () => {
+
+  const userAppointment: AppointmentProps[] = useSelector((state: RootState) => state.appointmentSlice).slice(1);
+  const appointmentToConfirm: AppointmentProps | null = userAppointment[userAppointment.length - 1];
+
+  const infosAppointment: string = `Me chamo ${appointmentToConfirm.name} ||  Cortar com ${appointmentToConfirm.barber} || ás ${appointmentToConfirm.hour} || Meu corte será ${appointmentToConfirm.cut} || e ${appointmentToConfirm.add} adicional.`;
+  const textToWpp: string = infosAppointment.replace(/ /g, '%20')
+  const sendMessage: string = `https://wa.me/5519984397784?text=${textToWpp}`
+
+  // const handleClickToConfirm = (): void => {
+
+  // }
+
   return (
-    <S.Wrapper>Confirm</S.Wrapper>
+    <S.Wrapper>
+      <Header/>
+      <S.Title>Confirmar</S.Title>
+      <S.Infos>
+        <p>
+          <strong>Seu nome:</strong> {appointmentToConfirm?.name}
+        </p>
+        <p>
+          <strong>Barbeiro:</strong> {appointmentToConfirm?.barber}
+        </p>
+        <p>
+          <strong>Corte:</strong> {appointmentToConfirm?.cut}
+        </p>
+        <p>
+          <strong>Adicionais:</strong> {appointmentToConfirm?.add}
+        </p>
+        <p>
+          <strong>Horário:</strong> {appointmentToConfirm?.hour}
+        </p>
+      </S.Infos>
+      {/* <S.Button onClick={handleClickToConfirm}>Marcar</S.Button> */}
+      <S.ButtonLink href={sendMessage} target="blank">Marcar</S.ButtonLink>
+    </S.Wrapper>
   )
 };
 
--- src/routes/Confirm/style.ts
+++ src/routes/Confirm/style.ts
@@ -2,4 +2,50 @@ import styled from "styled-components";
 
 
 export const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 800px;
+  margin: 0 auto;
+  padding-bottom: 100px;
 `;
+
+export const Title = styled.h1`
+  font-size: 2rem;
+  margin: 50px 0 20px;
+`;
+
+export const Infos = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 50px;
+  gap: 15px;
+
+  p {
+    font-size: 1.5rem;
+  }
+`;
+
+export const Button = styled.button`
+  width: 200px;
+  padding: 15px 0;
+  border: none;
+  background: #3B82F6;
+  color: white;
+  border-radius: 7px;
+  margin-top: 50px;
+  cursor: pointer;
+`
+export const ButtonLink = styled.a`
+  display: flex;
+  justify-content: center;
+  text-decoration: none;
+  width: 200px;
+  padding: 15px 0;
+  border: none;
+  background: #3B82F6;
+  color: white;
+  border-radius: 7px;
+  margin-top: 50px;
+  cursor: pointer;
+`
