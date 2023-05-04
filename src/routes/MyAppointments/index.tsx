cb63eb816fa06fcfc4ff14e5453c59785ab19bb2
Pinelli <ricardosilvapinelli@gmail.com>
2023-05-04 12:21:45 -0300

    MyAppointment: New page

---
+++ src/routes/MyAppointments/index.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../../components"
+import { Wrapper } from "./style"
+
+const MyAppointments = () => {
+  return (
+    <Wrapper>
+      <Header backToHome/>
+      <h1>Meus horários:</h1>
+    </Wrapper>
+  )
+}
+
+export default MyAppointments
---
+++ src/routes/MyAppointments/style.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+  width: 1128px;
+  margin: 0 auto;
+`
