66a3490b9105441068b7b715111d165be6196294
Pinelli <ricardosilvapinelli@gmail.com>
2023-05-05 11:21:55 -0300

    MyAppointments: code & design

--- src/routes/MyAppointments/index.tsx
+++ src/routes/MyAppointments/index.tsx
@@ -1,12 +1,42 @@
-import { Header } from "../../components"
-import { Wrapper } from "./style"
+import { Header } from "../../components";
+import { useSelector } from 'react-redux';
+import * as S from "./style";
+import { RootState } from "../../redux/rootStore";
+import { RiScissors2Line } from 'react-icons/ri';
+import { HiPlusSm } from 'react-icons/hi';
 
 const MyAppointments = () => {
+
+  const myAppointments = useSelector((state: RootState) => state.appointmentSlice).slice(1);
+  console.log(myAppointments)
+
   return (
-    <Wrapper>
+    <S.Wrapper>
       <Header backToHome/>
-      <h1>Meus horários:</h1>
-    </Wrapper>
+      <S.Content>
+        <span>Bem-vindo(a) {myAppointments[0]?.name}</span>
+        <S.Title>Meus horários</S.Title>
+        { myAppointments.length === 0 ? 
+          <S.Nothing>Sem horários</S.Nothing>
+          :
+          <>{
+            myAppointments.map((item) => (
+              <div className="card-appointment">
+                <div className="infos">
+                  <span className="barber-title">Barbeiro: {item.barber}</span>
+                  <h1 className="cut-title"><RiScissors2Line/> {item.cut}</h1>
+                  <h1 className="add-title"><HiPlusSm/> {item.add}</h1>
+                </div>
+                <div className="date">
+                  <span className="day-title">{item.day}</span>
+                  <span className="hour-title">{item.hour}</span>
+                </div>
+              </div>
+            ))
+          }</>
+        }
+      </S.Content>
+    </S.Wrapper>
   )
 }
 
--- src/routes/MyAppointments/style.ts
+++ src/routes/MyAppointments/style.ts
@@ -3,4 +3,82 @@ import styled from "styled-components";
 export const Wrapper = styled.section`
   width: 1128px;
   margin: 0 auto;
+  `
+
+export const Title = styled.h1`
+  font-size: 2rem;
+  margin-bottom: 20px;
 `
+
+export const Nothing = styled.span`
+  color: #9a9a99;
+  margin: 7rem auto 0;
+`
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 800px;
+  margin: 3rem auto 0;
+  gap: 15px;
+
+  .card-appointment {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    border-radius: 7px;
+    padding: 3rem;
+    width: 100%;
+    background: #d4d4d8;
+    filter: drop-shadow(0 8px 5px rgb(0 0 0 / 0.08));
+
+    .infos {
+      display: flex;
+      flex-direction: column;
+
+      .barber-title {
+        font-size: 0.9rem;
+        color: #475569;
+        margin-bottom: 1.4rem;
+      }
+      
+      .cut-title {
+        font-size: 2rem;
+        color: #0f172a;
+      }
+  
+      .add-title {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        font-size: 1.2rem;
+        color: #334155;
+        margin-top: 1rem;
+        font-weight: normal;
+  
+        svg {
+          margin-right: .5rem;
+        }
+      }
+    } // infos
+
+    .date {
+      display: flex;
+      flex-direction: column;
+      align-items: end;
+
+      .day-title {
+        font-size: 1.5rem;
+        font-weight: 700;
+        color: #0f172a;
+      }
+      .hour-title {
+        font-size: 1.3rem;
+        font-weight: 500;
+        color: #0f172a;
+      }
+    }
+
+  }
+`;
